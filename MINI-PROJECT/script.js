2bd95229c1c6a672e5cedc5e3560d711cd69f5ff
VINEET CHAUDHARY <mysteriousid2573@gmail.com>
2025-02-03 23:27:52 +0530

    Add files via upload

---
+++ MINI-PROJECT/script.js
@@ -0,0 +1,25 @@
+// Light/Dark toggle
+const checkbox = document.getElementById("checkbox");
+
+function introAboutLogoTransition() {
+    $("#about-quad").css("top", "70%");
+    $("#about-quad").css("opacity", "1");
+}
+
+function checkDarkMode() {
+    if (
+        localStorage.getItem("tourism_website_darkmode") !== null &&
+        localStorage.getItem("tourism_website_darkmode") === "true"
+    ) {
+        document.body.classList.add("dark");
+        checkbox.checked = true;
+    }
+}
+checkDarkMode();
+
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark");
+    document.body.classList.contains("dark")
+        ? localStorage.setItem("tourism_website_darkmode", true)
+        : localStorage.setItem("tourism_website_darkmode", false);
+});
